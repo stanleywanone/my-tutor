ad27ce661dd2fb47ca578a3f97866585df14e3ca
b6i0l5l <b6i0l5l@mail.fresnosate.edu>
2022-05-30 21:32:00 -0500

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 import styles from "../styles/Home.module.css"
 
 const Home: NextPage = () => {
-  c
+  x
   return (
     <div className={styles.container}>
       <Head>
