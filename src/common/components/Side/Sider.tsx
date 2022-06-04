83d7d6e175d639b3ec27812d0335eac7acfb3524
b6i0l5l <b6i0l5l@mail.fresnosate.edu>
2022-06-03 23:05:00 -0500

    side css with parent child

--- src/common/components/Side/Sider.tsx
+++ src/common/components/Side/Sider.tsx
@@ -11,6 +11,16 @@ export const Side: FC<any> = () => {
     { value: '/firstgrade/mathmap', label: 'Math Map', level: 'child', parent: '/firstgrade' },
   ];
 
+  const levelCondition = (item: any): void => {
+    if (item.level === 'parent') {
+      if (openMenu === '') setOpenMenu(item.value);
+      if (openMenu === item.value) setOpenMenu('');
+    } else {
+      if (openMenu !== item.parent) setOpenMenu('');
+      router.push(item.value);
+    }
+  };
+
   return (
     <div className="side-container">
       <h1 className="side-title">Mytutor</h1>
@@ -21,17 +31,22 @@ export const Side: FC<any> = () => {
               className="side-item"
               style={{
                 backgroundColor: router.pathname === item.value ? '#fdd85d' : undefined,
+                justifyContent: item.level === 'child' ? 'center' : undefined,
               }}
               key={item.label}
-              onClick={() => router.push(item.value)}
+              onClick={() => levelCondition(item)}
             >
-              <p>{item.label}</p>{' '}
-              {item.level === 'parent' && openMenu === '' && (
-                <IoMdArrowDropdown onClick={() => setOpenMenu(item.value)} />
-              )}
-              {item.level === 'parent' && openMenu === item.value && (
-                <IoMdArrowDropup onClick={() => setOpenMenu('')} />
-              )}
+              <div
+                className="side-item-content"
+                style={{
+                  paddingLeft: item.level === 'child' ? '0px' : '10px',
+                }}
+              >
+                {' '}
+                <p>{item.label}</p>{' '}
+                {item.level === 'parent' && openMenu === '' && <IoMdArrowDropdown />}
+                {item.level === 'parent' && openMenu === item.value && <IoMdArrowDropup />}
+              </div>
             </div>
           )
         );
